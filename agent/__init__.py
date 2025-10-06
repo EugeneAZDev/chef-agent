590d33ab4747b60f312312be2c8bf64467633f19
EugeneAZDev <eugene.zolotkov@gmail.com>
2025-10-06 21:24:37 +0300

    feat: add agent and use cases structure

---
+++ agent/__init__.py
@@ -0,0 +1,5 @@
+"""
+Agent package.
+
+This package will contain the LangGraph agent implementation.
+"""
---
+++ use_cases/__init__.py
@@ -0,0 +1,5 @@
+"""
+Use cases package.
+
+This package will contain business use cases and application services.
+"""
